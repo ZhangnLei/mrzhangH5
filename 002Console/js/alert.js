89aa59fc409b31d35cc65a4e6ce3c25cf87c1b1c
张念磊 <1822373498@qq.com>
2018-01-20 20:42:40 +0800

    alert

---
+++ 002Console/js/alert.js
@@ -0,0 +1,18 @@
+function guess() {
+    alert("你猜一会会发生什么");
+    alert("再等等");
+    alert("也许会有奇迹");
+}
+function guessAgain() {
+    guess();
+    alert("你相信奇迹吗?")
+    alert("让我来问你个问题")
+    guess();
+    alert("果然没有奇迹")
+    alert("我们就这样放弃吗?")
+    guess();
+}
+alert("你怎么打开这个网页了?");
+guessAgain();
+guessAgain();
+alert("这是最后一句了,拜拜")
